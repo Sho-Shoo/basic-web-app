b882a5b791d78f5faf0020ce20148c664c848bcf
ala <ala@aladembp.wifi.local.cmu.edu>
2023-03-14 16:18:34 -0400

    m

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -13,11 +13,24 @@ export default function QueryProcessor(query: string): string {
 
   if (query.toLowerCase().includes("plus")) {
     var str_arr = query.split("plus");
-    var num1 = parseInt(str_arr[0].replace(/\D/g, '')); 
-    var num2 = parseInt(str_arr[1].replace(/\D/g, '')); 
+    var num1 = parseInt(str_arr[0].replace(/\D/g, ''));
+    var num2 = parseInt(str_arr[1].replace(/\D/g, ''));
 
     return (num1 + num2).toString();
   }
+
+  if (query.toLowerCase().includes("cube")) {
+    var str_arr = query.split(",");
+    var len=str_arr.length;
+    var i;
+    for (i=0;i<len;i++){
+      var num=parseInt(str_arr[i].replace(/\D/g, ''))
+      if (Math.sqrt(num)%1==0&&Math.cbrt(num)%1==0)
+      return num.toString();
+    }
+    return "";
+  }
+
 
   return "";
 }
